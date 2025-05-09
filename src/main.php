2c167fed61b867100f44403eb57fe4f82617cc4e
susuko <flokinyan@gmail.com>
2025-05-09 22:28:57 +0900

    Update main.php

--- src/main.php
+++ src/main.php
@@ -1,3 +1,15 @@
 <?php
 
+/**
+ * A short description.
+ *
+ * PHP version 8.3
+ *
+ * @category Category
+ * @package  Package
+ * @author   Author <author@example.com>
+ * @license  example.com license
+ * @link     link
+ */
+
 echo "Hello, world!\n";
